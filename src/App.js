bc163a7b14c309641638fc0dcd007200fc0a9d2a
Julio Quintana <jquintana1801@gmail.com>
2018-06-17 15:57:06 -0300

    Git initial for sass

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './sass/App.css';
+import Button from './components/Button.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+
+const Button = () => (
+  <Fragment>
+    <button>
+      I am a button.
+    </button>
+  </Fragment>
+);
+
+export default Button;
